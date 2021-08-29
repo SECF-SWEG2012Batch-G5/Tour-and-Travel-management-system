0c93df2f7272b2f1752a7941b1d74e0da5d1d9e9
Tesnim19 <87711276+Tesnim19@users.noreply.github.com>
2021-08-29 20:16:23 +0300

    Update tour and travel.cxx

--- tour and travel.cxx
+++ tour and travel.cxx
@@ -38,3 +38,52 @@ int main()
 
 
 }
+void menu()
+   {
+       int p,s,pk;
+        int choice;
+        int count=0;
+       info inform[100];
+    menu:
+
+    cout<<endl<< setw(52)<< " select 1 to book tour "<< endl;
+    cout<<endl<< setw(52)<< " select 2 to edit "<< endl;
+    cout<<endl<< setw(52)<< " select 3 to search "<< endl;
+    cout<< endl<<setw(45)<< "select 4 to input rating  "<< endl;
+    cout<< endl<< setw(45)<< " select 5 to see record "<<endl;
+    cout<<endl<< setw(45)<< "select 6 to see reports "<< endl;
+    cout<< endl<<setw(48)<< "select 7 to see information"<< endl;
+
+    cin>> choice;
+     if (choice == 1)
+    {
+        count++;
+        getinfo(inform,count);
+
+          goto menu;
+
+
+
+    }else if (choice ==2)
+    {
+
+    }else if(choice== 3)
+    {
+
+    }else if (choice ==4 )
+    {
+
+    }else if (choice ==5 )
+    {
+        display(inform, p, s, pk);
+    }
+    else if (choice ==6 )
+    {
+    }
+    else{
+        cout<< "invalid choice! enter again"<< endl;
+       goto menu;
+    }
+
+
+    }
