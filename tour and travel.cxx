85dc78025207a2bf7133fd6d100166a91b0c815b
Tesnim19 <87711276+Tesnim19@users.noreply.github.com>
2021-09-05 22:11:36 +0300

    Update tour and travel.cxx

--- tour and travel.cxx
+++ tour and travel.cxx
@@ -1,115 +1,100 @@
-#include <iostream>
-#include <iomanip>
-#include <string.h>
+#include<iostream>
+#include<string.h>
 using namespace std;
+struct dep_date{
+	int dd,mm,yy;
+}; // structure to store date of departure
+struct rating{
+	string name;
+	int rate;
+}; //structure to store rating and the rater
+struct Dest {
+    string name,season,duration;
+	dep_date date;
+	rating ranks;
+    int sits,reserved,offer;
+    float vprice,nprice;
+}sites[100]; // structure to store info of sites
 
-struct place{
-
-
-};
-
-struct info{
-
-   string name;
-   int age;
-   string phone;
-   string email;
-   string season[2]={"summer","winter"};
-   string package[2]={"VIP","NORMAL"};
-   string place[3]={"GONDER","AXUM","HARAR"};
-   int id;
-
-
-};
-
- void menu();
- void getinfo(info[],int);
- void display(info[], int, int,int );
-
-int main()
-
-{
-
-    cout<<setw(70)<< "******************************************************"<< endl;
-    cout<< setw(50)<<"WELCOME "<< endl;
-    cout<< setw(70)<<"******************************************************"<< endl;
-     cout<< setw(40)<< "ANIT tour and travel agency "<<endl;
-   menu();
+class User{
+    public:
+        string name;
+        int age;
+        Dest package;
+        int id=100, visitorp = 0;
+}users[100]; // class to register users info
 
 
-}
-void menu()
-   {
-       int p,s,pk;
-        int choice;
-        int count=0;
-       info inform[100];
-    menu:
+Dest newSite(){
+    Dest site;
+            cout<<"Enter site name: ";
+            cin>>site.name;
+            cout<<"Enter travel season: ";
+            cin>>site.season;
+            cout<<"Enter number of passengers: ";
+            cin>>site.sits;
+            cout<<"Enter duration of the tour: ";
+            cin>>site.duration;
+            cout<<"Enter date of departure(dd mm yy): ";
+            cin>>site.date.dd>>site.date.mm>>site.date.yy;
+            cout<< "enter price for VIP package"<<endl;
+    		cin>> site.vprice;
+   			cout<< "enter price for normal package"<< endl;
+    		cin>> site.nprice;
 
-    cout<<endl<< setw(52)<< " select 1 to book tour "<< endl;
-    cout<<endl<< setw(52)<< " select 2 to edit "<< endl;
-    cout<<endl<< setw(52)<< " select 3 to search "<< endl;
-    cout<< endl<<setw(45)<< "select 4 to input rating  "<< endl;
-    cout<< endl<< setw(45)<< " select 5 to see record "<<endl;
-    cout<<endl<< setw(45)<< "select 6 to see reports "<< endl;
-    cout<< endl<<setw(48)<< "select 7 to see information"<< endl;
+        return site;
+} // function for site registering by the admin
 
-    cin>> choice;
-     if (choice == 1)
+void del(int &j){
+    int id,x;
+    cout<<"\n.......CANCEL RESERVATION......"<<endl;
+    cout<<"\nEnter id: ";
+    cin>>id;
+
+    for (int i=0; i<j; i++)
     {
-        count++;
-        getinfo(inform,count);
-
-          goto menu;
-
-
-
-    }else if (choice ==2)
-    {
-
-    }else if(choice== 3)
-    {
-
-    }else if (choice ==4 )
-    {
-
-    }else if (choice ==5 )
-    {
-        display(inform, p, s, pk);
+      if(id==users[i].id)
+      {
+        x=i;
+        break;
+      }
     }
-    else if (choice ==6 )
+    for(int i=x+1; i<j; i++)
     {
-    }
-    else{
-        cout<< "invalid choice! enter again"<< endl;
-       goto menu;
-    }
+      users[i-1]=users[i];
+  }
+  --j;
+    cout<<"\nReservation if user with id "<<id<< "is canceled."<<endl;
+  }     // function for deleting/cancelling a reservation
 
+void vipp(int no_dest, int j, string dest_name, int user_id){
+   	int n1,n2;
 
-    }
- void getinfo(info inform[],int count){
-      string cplace;
-       int p,s,pk;
-       cout<<"Enter name: ";
-       cin>>inform[count].name;
+   	for(int i=0; i<no_dest; i++)
+	   {
+	   	if(dest_name==sites[i].name)
+		  {
+	   		n1=i;
+	      }
+	   } // ending of the loop that checks for matching name of destination
+	for(int i=0; i<j; i++)
+	   {
+		if(user_id+j==users[i].id)
+		  {
+	   		n2=i;
+		  }
+       }// ending of the for loop that checks for matching user id
 
-       cout<<"Enter age: ";
-       cin>>inform[count].age;
+      cout<< "\nPrice for vip package to "<< sites[n1].name << " is "<< sites[n1].vprice<<endl;
+      if ( users[n2].visitorp == 3)
+       {
+         cout<< " you have a 50 % discount because this is your third trip with us. thank you for being a loyal customer "<<endl;
+	        cout<< "your new price is: "<<sites[n1].vprice * 0.5<<endl;
+        users[n2].visitorp = 0;
+       } // ending of if statement for checking visitor point to place discount
 
-       cout<<"Enter email: ";
-       cin>>inform[count].email;
+   } // function for displaying price for VIP package
 
-       cout<<"Enter phone: ";
-       cin.ignore();
-       getline(cin,inform[count].phone,'\n');
+void normp(int no_dest, int j, string dest_name, int user_id){
+   	int n1,n2;
 
-
-       cout<<"Enter destination of choice: "<<endl;
-
-       cout<<"0 for Gonder "<<endl;
-       cout<<"1 for Axum "<<endl;
-       cout<<"2 for Harar"<<endl;
-       cin>>p;
-        cin.ignore();
-       inform[count].place[p];
- }
